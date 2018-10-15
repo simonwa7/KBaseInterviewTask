2a77745696737a1c9a84b9c6364aebb576418877
simonwa7 <simonwa7@gmail.com>
2018-10-15 16:49:31 -0400

    sorted data for general election contributions

--- election-donations/src/app/data/data.component.ts
+++ election-donations/src/app/data/data.component.ts
@@ -18,35 +18,39 @@ export class DataComponent implements OnInit {
       var reader = new FileReader();
       reader.readAsText(event.target.files[0]);
       var me = this;
-  
-      reader.onload = function (ev) {
-        // console.log(ev.target.result)
-  
-        // var allTextLines = ev.target.result.split(/\r\n|\n/);
+
+      reader.onload = function (ev: any) {
+
+        var allTextLines = ev.target.result.split(/\r\n|\n/);
             
-            // for (var i=0; i<allTextLines.length; i++) {
-            //     var data = allTextLines[i].split('|');
-            //         var tarr = [];
-            //         for (var j=0; j<data.length; j++) {
-            //             tarr.push(data[j]);
-            //         }
-            //         if(tarr[3] == "G"){
-            //           me.donations.push(tarr);
-            // }
-            // me.donations.push(tarr)
+          for (var i=0; i<allTextLines.length; i++) {
+              var data = allTextLines[i].split('|');
+                  var tarr = [];
+                  for (var j=0; j<data.length; j++) {
+                      tarr.push(data[j]);
+                  }
+                  if(tarr[3] == "G"){
+                    me.donations.push(tarr);
+                  }
+          }
+
+          console.log(me.donations)
+          me.sortDonations()
+          console.log(me.donations)
+
         }
-      }
+    }
   
-    // sortDonations(){
-    //   this.donations.sort( function(candidate1, candidate2) {
-    //     if ( candidate1[7] < candidate2[7] ){
-    //       return -1;
-    //     }else if( candidate1[7] > candidate2[7] ){
-    //       return 1;
-    //     }else{
-    //       return 0;
-    //     }
-    //   });
-    // }
+    sortDonations(){
+      this.donations.sort( function(candidate1, candidate2) {
+        if ( candidate1[7] < candidate2[7] ){
+          return -1;
+        }else if( candidate1[7] > candidate2[7] ){
+          return 1;
+        }else{
+          return 0;
+        }
+      });
+    }
 
 }
