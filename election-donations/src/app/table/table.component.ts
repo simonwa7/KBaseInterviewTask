5e8e61667b2ed4ab1b1cb8c408d41ec0fde8d64e
simonwa7 <simonwa7@gmail.com>
2018-10-16 12:28:20 -0400

    table component successfully takes candidate data on input

--- election-donations/src/app/table/table.component.ts
+++ election-donations/src/app/table/table.component.ts
@@ -6,11 +6,18 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./table.component.css']
 })
 export class TableComponent implements OnInit {
-	@Input('title') title: string;
+  @Input('title') title: string;
+  @Input('dataTitle') dataTitle: string;
+  @Input('data') data: any[];
+  headers: string[] = [];
   
   constructor() { }
 
   ngOnInit() {
+    if(this.dataTitle == "Candidates"){
+      this.headers = ["ID", "Candidate Name(s)", "Party", "Donations Raised"]
+    }
+
   }
 
 }
